d846896786b7f49d0e5d237e44c8d4159f490cc3
LeeKLTW <LeeKLTW@gmail.com>
2019-03-29 19:03:22 +0800

    run hello world

--- socketIO_approach/main.py
+++ socketIO_approach/main.py
@@ -10,3 +10,14 @@ socketio = SocketIO(app)
 @app.route('/')
 def session():
     return render_template('session.html')
+
+def messageRecieved(methods=['GET','POST']):
+    print('Message recieved!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET','POST']):
+    print('Recieved event:'+str(json))
+    socketio.emit('My response', json ,callback=messageRecieved)
+
+if __name__ == '__main__':
+    socketio.run(app,debug=True)
