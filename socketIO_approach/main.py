37800f5d1b60d4be098718fc5e98fbb7963160d1
LeeKLTW <LeeKLTW@gmail.com>
2019-03-29 18:56:37 +0800

    session template

--- socketIO_approach/main.py
+++ socketIO_approach/main.py
@@ -1,7 +1,12 @@
 # encoding: utf-8
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRETE_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
+
+
+@app.route('/')
+def session():
+    return render_template('session.html')
